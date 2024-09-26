971aa35bc93b55f2d5d5b0e27062356693b692cf
Himasopn <129391898+Himasopn@users.noreply.github.com>
2024-09-27 01:22:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "Pika",
+    ownernumber: process.env.Owner_Number || "79149958622",
+    instagramId: process.env.Insta || "woxsov",
 
     /**
      * other details and parameters
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "Ynhnbmt_AN_YA_4eWI=",
 
     /**
      * thumbnails and medias
